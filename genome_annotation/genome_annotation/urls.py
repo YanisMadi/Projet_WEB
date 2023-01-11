27940e93073be4131530de57760dcea10f637aec
Clarpalmtree <claraa.toussaint@gmail.com>
2023-01-11 10:51:47 +0100

    Page html

--- genome_annotation/genome_annotation/urls.py
+++ genome_annotation/genome_annotation/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('annotations/', views.annotation_list, name='annotation_list'),
     path('menu/', views.annot_menu, name='annot_menu'),
     path('login/', views.login_view, name='login_view'),
+    path('inscription/', views.inscription, name = 'inscription'),
 ]
