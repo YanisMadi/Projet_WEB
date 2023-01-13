b22c841d5474518cc2a9444416444b3a4937a1f5
plim <pauline.lim0299@gmail.com>
2023-01-13 18:35:33 +0100

    genome

--- genome_annotation/genome/views.py
+++ genome_annotation/genome/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import GenomeAnnotation
+from .models import Genome
 
 def annotation_list(request):
     # Récupérer toutes les annotations de génome de la base de données
-    annotations = GenomeAnnotation.objects.all()
+    annotations = Genome.objects.all()
     # Créer un contexte de données à passer au template
     context = {'annotations': annotations}
     # Rendre le template avec le contexte de données
