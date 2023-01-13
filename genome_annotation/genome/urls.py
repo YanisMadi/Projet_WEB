a7b3606848edb045db6709341bfb5aa20a3394fb
plim <pauline.lim0299@gmail.com>
2023-01-13 16:47:09 +0100

    generalise url pattern

--- genome_annotation/genome/urls.py
+++ genome_annotation/genome/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('annotations/', views.annotation_list, name='annotation_list'),
-    path('menu/', views.annot_menu, name='annot_menu'),
-    path('login/', views.login_view, name='login_view'),
-    path('inscription/', views.inscription, name = 'inscription'),
+    path('./annotations/', views.annotation_list, name='annotation_list'),
+    path('./menu/', views.annot_menu, name='annot_menu'),
+    path('./login/', views.login_view, name='login_view'),
+    path('./inscription/', views.inscription, name = 'inscription'),
+
 ]
 
 from django.urls import path
