a5462882f74ca05516f99ebd0a3a23dc3d4f5d33
Yanis Madi <95629416+YanisMadi@users.noreply.github.com>
2023-01-17 20:02:54 +0100

    Update inscr

---
+++ genome_annotation/genome/forms/inscription_form.py
@@ -0,0 +1,7 @@
+from django import forms
+from ..models import User
+
+class InscriptionForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'prenom', 'nom', 'numero_tel', 'role', 'motdepasse','confirm_mdp']
