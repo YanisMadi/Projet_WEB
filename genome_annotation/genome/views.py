94591a488827ad905cc27f4c92daa78931985782
Yanis Madi <95629416+YanisMadi@users.noreply.github.com>
2023-01-17 20:03:23 +0100

    Update incr

--- genome_annotation/genome/views.py
+++ genome_annotation/genome/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from .models import Annotations
+from .models import Annotations, User
+from .forms.inscription_form import InscriptionForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 def annotation_list(request):
     # Récupérer toutes les annotations de génome de la base de données
@@ -19,11 +24,39 @@ def login_view(request):
         'css_files': ['login.css'],
     })
 
-def inscription(request) :
+def inscription(request):
+    if request.method == 'POST':
+        form = InscriptionForm(request.POST)
+        if form.is_valid():
+            # Vérification que l'adresse email n'est pas déjà utilisée
+            if User.objects.filter(email=form.cleaned_data['email']).exists():
+                form.add_error('email', 'Cette adresse email est déjà utilisée.')
+            # Vérification que les mots de passe correspondent
+            elif form.cleaned_data['motdepasse'] != form.cleaned_data['confirm_mdp']:
+                form.add_error('motdepasse', 'Les mots de passe ne correspondent pas.')
+                form.add_error('confirm_mdp', 'Les mots de passe ne correspondent pas.')
+            else:
+                # Enregistrement des données d'inscription dans la base de données
+                user = form.save()
+                messages.success(request, "Votre inscription a été effectuée avec succès.")
+                # Envoi d'un email de confirmation
+                send_mail(
+                    'Confirmation d\'inscription',
+                    'Merci de vous être inscrit sur notre site.',
+                    'from@example.com',
+                    [user.email],
+                    fail_silently=False,
+                )
+                # Redirection vers une autre page
+                return redirect('menu')
+    else:
+        form = InscriptionForm()
 
     return render(request, 'genome/inscription.html', {
+        'form': form,
         'css_files': ['Inscription.css'],
     })
+
 
 def formulaire_genome(request) :
 
