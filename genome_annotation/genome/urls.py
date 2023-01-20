3981d9efb3a7bd1764d40a9ac47a67049134d92a
plim <pauline.lim0299@gmail.com>
2023-01-20 16:58:31 +0100

    connexion des urls.py

--- genome_annotation/genome/urls.py
+++ genome_annotation/genome/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('./annotations/', views.annotation_list, name='annotation_list'),
-    path('./', views.annot_menu, name='annot_menu'),
-    path('./login/', views.login_view, name='login_view'),
-    path('./inscription/', views.inscription, name = 'inscription'),
-    path('./formulaire/', views.formulaire_genome, name = 'formulaire'),
+    path('annotations/', views.annotation_list, name='annotation_list'),
+    path('', views.annot_menu, name='annot_menu'),
+    path('login/', views.login_view, name='login_view'),
+    path('inscription/', views.inscription, name = 'inscription'),
+    path('formulaire/', views.formulaire_genome, name = 'formulaire'),
 
 ]
-
 
