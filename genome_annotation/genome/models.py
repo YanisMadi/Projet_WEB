5fd1ac5969e6239766b1d47d519ed016a6818a36
Yanis Madi <95629416+YanisMadi@users.noreply.github.com>
2023-01-24 15:44:48 +0100

    Update models.py

--- genome_annotation/genome/models.py
+++ genome_annotation/genome/models.py
@@ -1,17 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.db import models
+from django.contrib.auth.hashers import check_password, make_password
 
-class User(models.Model):
+class UserManager(UserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Les utilisateurs doivent avoir un email")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-    ROLES = [('admin', 'Admin'),('lecteur', 'Lecteur'),('annotateur', 'Annotateur'),('valideur', 'Valideur')]
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_admin = True
+        user.save()
+        return user
 
-    # email de l'utilisateur
-    email = models.TextField(primary_key=True, blank=False)
+class User(AbstractBaseUser):
+    ROLES = [('admin', 'Admin'), ('lecteur', 'Lecteur'), ('annotateur', 'Annotateur'), ('valideur', 'Valideur')]
+    email = models.EmailField(primary_key=True, unique=True)
     prenom = models.CharField(max_length=100)
     nom = models.CharField(max_length=100)
     numero_tel = models.IntegerField()
     role = models.TextField(choices=ROLES,default='lecteur')
-    motdepasse = models.TextField(null=False)
-    confirm_mdp = models.TextField(null=False)
+    password = models.CharField(null=False,max_length=128)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
+    objects = UserManager()
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+        
+    @property
+    def is_active(self):
+        return self.is_active
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+    
+    def set_is_active(self, value):
+        self.is_active = value
+        self.save()
+    
+    def get_username(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.prenom
+
+    def get_full_name(self):
+        return self.prenom + ' ' + self.nom
+
+
+
+
+
+        
+
+
+
+
 
 
 class Genome(models.Model):
