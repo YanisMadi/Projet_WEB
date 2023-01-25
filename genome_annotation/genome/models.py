9589713c737f4a6c8f019cb20e5bd49a68602f21
plim <pauline.lim0299@gmail.com>
2023-01-25 10:39:21 +0100

    models modification

--- genome_annotation/genome/models.py
+++ genome_annotation/genome/models.py
@@ -15,36 +15,46 @@ class User(models.Model):
 
 
 class Genome(models.Model):
+    DNA_TYPE = [('chr', 'chromosome'),('plm', 'plasmide')]
+
     num_accession = models.IntegerField(primary_key=True, blank=False)
-    nom_gene = models.CharField(max_length=50)
+    #nom_gene = models.CharField(max_length=50)
     espece = models.CharField(max_length=50)
-    souche = models.CharField(max_length=50)
+    #souche = models.CharField(max_length=50)
+    type_adn = models.CharField(choices=DNA_TYPE,default='chromosome',max_length=10)
     sequence = models.TextField()
     longueur = models.PositiveIntegerField()
-    description = models.CharField(max_length=1000)
+    #description = models.CharField(max_length=1000)
 
 
 class SequenceInfo(models.Model):
 
-    DNA_TYPE = [('chr', 'chromosome'),('plm', 'plasmide')]
-    STRAND_TYPE = [('backward','-1'),('forward','+1')]
+    
+    #STRAND_TYPE = [('backward','-1'),('forward','+1')]
     ANNOTATION = [('oui','annoté'),('no,','non annoté')]
 
-    num_accession = models.ForeignKey(Genome,on_delete=models.CASCADE)
-    type_adn = models.CharField(choices=DNA_TYPE,default='chromosome',max_length=10)
-    start = models.IntegerField()
-    end = models.IntegerField()
-    strand = models.CharField(choices=STRAND_TYPE,default='forward',max_length=8)
+    num_accession = models.CharField(max_length=30) # genome_id
+    type_adn = models.CharField(max_length=30)
+    seq_id = models.CharField(max_length=30, primary_key=True)
+    seq_name = models.CharField(max_length=30)
+    seq_type = models.CharField(max_length=30)
+    seq_biotype = models.CharField(max_length=30)
+    fonction = models.CharField(max_length=100)
+    seq_start = models.IntegerField()
+    seq_end = models.IntegerField()
+    sequence = models.TextField()
+    longueur = models.IntegerField()
     annotated_state = models.CharField(choices=ANNOTATION, default='non annoté',max_length=10)
     
 
 class Annotations(models.Model):
-
+    
     STATUS = [('validé','val'),('en cours', 'en attente'),('rejeté', 'rej')]
 
     annot_id = models.IntegerField(primary_key=True, blank=False)
     email_annot = models.ForeignKey(User,on_delete=models.CASCADE,max_length=100)
-    genome_ID = models.ForeignKey(Genome,on_delete=models.CASCADE,max_length=50)
-    Biotype = models.CharField(max_length=100)
-    comments = models.CharField(max_length=1000)
+    num_accession = models.ForeignKey(Genome,on_delete=models.CASCADE,max_length=50)
+    seq_biotype = models.CharField(max_length=100)
+    fonction = models.CharField(max_length=100)
+    strand = models.CharField(choices=STRAND_TYPE,default='forward',max_length=8)
     annotation_status = models.CharField(choices=STATUS,default='en attente',max_length=10)
