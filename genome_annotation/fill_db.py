3cea031f384df39e0095bd3e8c35841c0f95bd25
Clarpalmtree <claraa.toussaint@gmail.com>
2023-01-25 12:05:29 +0100

    commit

--- genome_annotation/fill_db.py
+++ genome_annotation/fill_db.py
@@ -9,28 +9,30 @@ data = dat.iterdir()
 
 for file in data : 
     file = str(file)
-    if ('cds' or 'pep') and not "new" in file : 
-        liSeq = ps.parsing_coding(file)
-        for i in range(len(liSeq)) :
-            genome_id, genome_type, specy, seq, len_seq = liSeq[i]
-            Genome(num_accession = genome_id, espece = specy, type_adn = genome_type, sequence = seq, longueur = len_seq).save()
-            
+    print(file)
+    if not "new" in file : 
+        if ('cds' or 'pep') in file : 
+            liSeq = ps.parsing_coding(file)
+            for i in range(len(liSeq)) :
+                gene_id,gene_name,gene_type,gene_biotype,description,start,end,genome_id,genome_type,seq, seq_length= liSeq[i]
+                SequenceInfo(seq_id = gene_id ,seq_name =gene_name ,seq_type = gene_type,seq_biotype=gene_biotype,
+                            fonction = description,seq_start=start,seq_end=end, num_accession = genome_id, 
+                            type_adn = genome_type, sequence=seq, longueur=seq_length ).save() 
+                
 
-    elif 'new' and ('cds' or 'pep') in file:
-        liSeq_new = ps.parsing_new(file) 
-        for i in range(len(liSeq_new)) : 
-            gene_id,gene_type,start,end,genome_id,genome_type,seq,seq_length = liSeq_new[i]
-            SequenceInfo(seq_id = gene_id, seq_type=gene_type, seq_start= start, seq_end = end, 
-                            num_accession=genome_id, type_adn=genome_type, sequence = seq, longueur = seq_length).save()
+    elif 'new' in file :
+        if ('cds' or 'pep') in file : 
+            liSeq_new = ps.parsing_new(file) 
+            for i in range(len(liSeq_new)) : 
+                gene_id,gene_type,start,end,genome_id,genome_type,seq,seq_length = liSeq_new[i]
+                SequenceInfo(seq_id = gene_id, seq_type=gene_type, seq_start= start, seq_end = end, 
+                                num_accession=genome_id, type_adn=genome_type, sequence = seq, longueur = seq_length).save()
 
     
     else :
 
         liSeq_g = ps.parsing_genome(file)
         for i in range(len(liSeq_g)) :
-            gene_id,gene_name,gene_type,gene_biotype,description,start,end,genome_id,genome_type,seq, longueur= liSeq_g[i]
-            SequenceInfo(seq_id = gene_id ,seq_name =gene_name ,seq_type = gene_type,seq_biotype=gene_biotype,
-                            fonction = description,seq_start=start,seq_end=end, num_accesion = genome_id, 
-                            type_adn = genome_type, sequence=seq, seq_lenght = longueur ).save() 
-
+            genome_id, genome_type, specy, seq, len_seq = liSeq_g[i]
+            Genome(num_accession = genome_id, espece = specy, type_adn = genome_type, sequence = seq, longueur = len_seq).save()
             
