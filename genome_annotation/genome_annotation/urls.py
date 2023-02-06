bd7d550520bd82f7a189fa0ff09e1713c8b59490
plim <pauline.lim0299@gmail.com>
2023-02-06 20:44:16 +0100

    genome_annotation urls

--- genome_annotation/genome_annotation/urls.py
+++ genome_annotation/genome_annotation/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from genome import urls
 from genome import views
+from django.urls import re_path
 
 urlpatterns = [
     path('annotations/', views.annotation_list, name='annotation_list'),
@@ -10,7 +11,6 @@ urlpatterns = [
     path('inscription/', views.inscription, name = 'inscription'),
     path('formulaire/', views.formulaire_genome, name = 'formulaire'),
     path('formulaire/view_sequence/', views.view_sequence, name="view_sequence"),
-    path('formulaire/view_genesequence/', views.view_genesequence, name="view_genesequence"),
     path('annotateur_page/', views.annotateur_page, name='annotateur_page'),
     path('validateur_page/', views.validateur_page, name='validateur_page'),
     path('lecteur_page/', views.lecteur_page, name='lecteur_page'),
@@ -18,4 +18,5 @@ urlpatterns = [
     path('manage_users/', views.manage_users, name='manage_users'),
     path('sequence/', views.show_sequences, name='sequence'),
     path('validation/', views.validate_annotation, name='validation'),
+    re_path('./view_genesequence/',  views.view_genesequence, name='view_genesequence'),
 ]
