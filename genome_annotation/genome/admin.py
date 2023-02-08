5d9283d9917fffee2cfdf57975da1610338f850d
YanisMadi <yanis_madi@yahoo.com>
2023-02-08 12:47:50 +0100

    Update pour encoder le mdp

--- genome_annotation/genome/admin.py
+++ genome_annotation/genome/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'prenom', 'nom', 'numero_tel', 'role', 'last_login', 'is_validated')
     ordering = ('username',)
-
+    
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.is_validated = False
-            obj.set_password(obj.password)
+        obj.set_password(form.cleaned_data["password"])
         obj.save()
+        return super().save_model(request, obj, form, change)
 
 admin.site.register(User, UserAdmin)
