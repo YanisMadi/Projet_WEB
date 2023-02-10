692e122821ae7797092eb13f013d2fd0f62a6a9a
plim <pauline.lim0299@gmail.com>
2023-02-10 22:57:37 +0100

    mot de passe encrypté

--- genome_annotation/genome/admin.py
+++ genome_annotation/genome/admin.py
@@ -4,12 +4,18 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'prenom', 'nom', 'numero_tel', 'role', 'last_login', 'is_validated')
     ordering = ('username',)
-    
+
     def save_model(self, request, obj, form, change):
+        if change:
+            # If the password has already been set and the user is being updated, don't re-encrypt it
+            form.cleaned_data.pop('password', None)
+        else:
+            obj.set_password(form.cleaned_data["password"])
+
         if not obj.pk:
             obj.is_validated = False
-        obj.set_password(form.cleaned_data["password"])
         obj.save()
         return super().save_model(request, obj, form, change)
+
 
 admin.site.register(User, UserAdmin)
